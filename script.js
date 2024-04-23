015de023bf361ad28cc5205502c21f7e851b0787
brummellucas <85232679+brummellucas@users.noreply.github.com>
2024-04-23 00:35:37 -0400

    retornando as horas

--- script.js
+++ script.js
@@ -181,7 +181,7 @@ checkoutBtn.addEventListener("click", function(){
 function checkRestaurantOpen(){
     const data = new Date();
     const hora = data.getHours();
-    //return hora >= 14 && hora <= 22; // boolean true/false
+    return hora >= 14 && hora <= 22; // boolean true/false
 
     
 }
