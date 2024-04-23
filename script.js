d77ddaa87bbdc2711ccb82174b24442abda05d64
deritne <162658483+deritne@users.noreply.github.com>
2024-04-23 00:45:41 -0400

    hora

--- script.js
+++ script.js
@@ -181,7 +181,7 @@ checkoutBtn.addEventListener("click", function(){
 function checkRestaurantOpen(){
     const data = new Date();
     const hora = data.getHours();
-    //return hora >= 01 && hora <= 22; // boolean true/false
+    //return hora >= 24 && hora <= 23; // boolean true/false
 
     
 }
